1f6f0feec00d1e132010fdadba52d5123fb43685
amigin <amigin@gmail.com>
2024-03-27 15:32:58 +0200

    Debugging new scope

--- src/google_auth/generate_login_page.rs
+++ src/google_auth/generate_login_page.rs
@@ -6,7 +6,7 @@ pub fn generate_login_page<THostPort: HostPort + Send + Sync + 'static>(
 ) -> String {
     return super::html::generate_with_template(|| {
         format!(
-            r###"<a class="btn btn-primary" href="https://accounts.google.com/o/oauth2/v2/auth?scope=https%3A//www.googleapis.com/auth/drive.metadata.readonly&access_type=offline&include_granted_scopes=true&response_type=code&state=state_parameter_passthrough_value&redirect_uri=https%3A//{}&client_id={}">Sign in with Google</a>"###,
+            r###"<a class="btn btn-primary" href="https://accounts.google.com/o/oauth2/v2/auth?scope=openid%20email&access_type=offline&include_granted_scopes=true&response_type=code&state=state_parameter_passthrough_value&redirect_uri=https%3A//{}&client_id={}">Sign in with Google</a>"###,
             super::generate_redirect_url(req),
             settings.client_id.as_str()
         )
