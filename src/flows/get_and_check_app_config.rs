4b109a55a9a7a2b86e8001b3aab935d5ce03113f
amigin <amigin@gmail.com>
2024-04-03 15:02:39 +0300

    Fixing with client certificate

--- src/flows/get_and_check_app_config.rs
+++ src/flows/get_and_check_app_config.rs
@@ -14,23 +14,20 @@ pub async fn get_and_check_app_config(app: &AppContext) -> Result<AppConfigurati
 
     for (listen_port, port_config) in &listen_ports {
         if let Some(ssl_cert_id) = port_config.get_ssl_certificate() {
-            if ssl_cert_id.as_str() == SELF_SIGNED_CERT_NAME {
-                continue;
-            }
-            if !ssl_certificates_cache.has_certificate(ssl_cert_id) {
-                let ssl_certificate =
-                    crate::flows::load_ssl_certificate(&settings_reader, ssl_cert_id, *listen_port)
-                        .await?;
-                ssl_certificates_cache.add(ssl_cert_id, ssl_certificate);
+            if ssl_cert_id.as_str() != SELF_SIGNED_CERT_NAME {
+                if !ssl_certificates_cache.has_certificate(ssl_cert_id) {
+                    let ssl_certificate = crate::flows::load_ssl_certificate(
+                        &settings_reader,
+                        ssl_cert_id,
+                        *listen_port,
+                    )
+                    .await?;
+                    ssl_certificates_cache.add(ssl_cert_id, ssl_certificate);
+                }
             }
         }
 
         for client_cert_id in port_config.get_client_certificates() {
-            println!(
-                "Port: {} has client certificate {}",
-                *listen_port,
-                client_cert_id.as_str()
-            );
             if !client_certificates_cache.has_certificate(client_cert_id) {
                 let client_certificate = crate::flows::load_client_certificate(
                     &settings_reader,
