c323b9c895c5093e50ce27aa4cdd8edecfd7adaf
amigin <amigin@gmail.com>
2024-11-02 00:57:45 +0200

    Removed unused lines

--- src/configurations/end_point_type.rs
+++ src/configurations/end_point_type.rs
@@ -7,29 +7,3 @@ pub enum EndpointType {
     Tcp(Arc<TcpEndpointHostConfig>),
     TcpOverSsh(Arc<TcpOverSshEndpointHostConfig>),
 }
-
-/*
-impl EndpointType {
-    pub fn get_my_http_locations(&self, req: &HttpRequestBuilder, is_https: bool) -> bool {
-        match self {
-            Self::Http(http_endpoint_info) => {
-                if req.is_mine(http_endpoint_info.host_endpoint.as_str(), is_https) {
-                    return true;
-                }
-            }
-
-            Self::Tcp(_) => {}
-            Self::TcpOverSsh(_) => {}
-        }
-        false
-    }
-
-    fn get_host(&self) -> &HostString {
-        match self {
-            EndpointType::Http(endpoint_info) => &endpoint_info.host_endpoint,
-            EndpointType::Tcp(endpoint_info) => &endpoint_info.host,
-            EndpointType::TcpOverSsh(endpoint_info) => &endpoint_info.host,
-        }
-    }
-}
- */
