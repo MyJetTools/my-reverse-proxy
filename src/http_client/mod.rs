b67a5cd83cb18541efaf8b5b234a9f22e6586c2f
amigin <amigin@gmail.com>
2024-11-02 00:59:23 +0200

    Unused files are removed

--- src/http_client/mod.rs
+++ src/http_client/mod.rs
@@ -1,26 +1,17 @@
 mod http1_client;
 pub use http1_client::*;
-//mod connect_to_http_endpoint;
-//pub use connect_to_http_endpoint::*;
 mod error;
 pub use error::*;
-//mod connect_to_tls_endpoint;
-//pub use connect_to_tls_endpoint::*;
 
-//mod connect_to_http_over_ssh;
-//pub use connect_to_http_over_ssh::*;
 mod connect_to_http2_endpoint;
 pub use connect_to_http2_endpoint::*;
 mod http2_client;
 pub use http2_client::*;
-//mod http_client;
-//pub use http_client::*;
+
 mod connect_to_http2_over_ssh;
 pub use connect_to_http2_over_ssh::*;
-//mod connect_to_http_unix_socket_endpoint;
 
 pub const HTTP_CLIENT_TIMEOUT: std::time::Duration = std::time::Duration::from_secs(30);
 mod connect_to_http2_unix_socket_endpoint;
 mod http1_over_ssh_client;
-mod unix_socket_utils;
 pub use http1_over_ssh_client::*;
