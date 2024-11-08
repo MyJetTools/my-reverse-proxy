890e8ebc99c6a0de42298d246f47430b136d2a5b
amigin <amigin@gmail.com>
2024-11-08 13:08:42 +0200

    Debugging Name generation

--- src/ssl/ssl_certificate.rs
+++ src/ssl/ssl_certificate.rs
@@ -54,7 +54,12 @@ impl SslCertificate {
             for attr in cert.subject().iter_attributes() {
                 // OID for Common Name
                 if let Ok(cn) = attr.as_str() {
-                    found_cn = Some(cn.to_string());
+                    let name = match found_cn {
+                        Some(found_cn) => format!("{}/", found_cn),
+                        _ => cn.to_string(),
+                    };
+
+                    found_cn = Some(name);
                 }
             }
         }
