5fe6ca1280082ee34456dfd8eedd806c18dd9fba
amigin <amigin@gmail.com>
2024-12-11 00:51:18 +0200

    Removed unused files

--- src/http_server/controllers/configuration/mod.rs
+++ src/http_server/controllers/configuration/mod.rs
@@ -1,7 +1,3 @@
-//mod test_config_action;
-//pub use test_config_action::*;
-//mod test_and_apply_action;
-//pub use test_and_apply_action::*;
 pub mod contracts;
 mod get_current_config_action;
 pub use get_current_config_action::*;
