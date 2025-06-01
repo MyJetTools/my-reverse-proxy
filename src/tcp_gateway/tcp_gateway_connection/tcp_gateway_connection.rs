90a4376736d906ea27ff1d5daebbb27cc3a0fd51
amigin <amigin@gmail.com>
2025-06-02 00:23:01 +0300

    Debugging

--- src/tcp_gateway/tcp_gateway_connection/tcp_gateway_connection.rs
+++ src/tcp_gateway/tcp_gateway_connection/tcp_gateway_connection.rs
@@ -373,6 +373,8 @@ impl TcpGatewayConnection {
     }
 
     pub async fn send_payload<'d>(&self, payload: &TcpGatewayContract<'d>) -> bool {
+        println!("Gateway. Send payload {:?}", payload);
+
         let supported_compression = self.get_supported_compression();
         let vec = payload.to_vec(&self.inner.aes_key, supported_compression);
 
