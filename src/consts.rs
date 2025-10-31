0a65ebe79bbc73327bf9d6497cde2e48be90b99e
amigin <amigin@gmail.com>
2025-10-31 19:50:24 +0200

    Increased Timeouts

--- src/consts.rs
+++ src/consts.rs
@@ -4,7 +4,7 @@ pub const DEFAULT_HTTP_REQUEST_TIMEOUT: std::time::Duration = std::time::Duratio
 
 pub const DEFAULT_HTTP_CONNECT_TIMEOUT: std::time::Duration = std::time::Duration::from_secs(5);
 
-pub const READ_TIMEOUT: Duration = Duration::from_secs(30);
+pub const READ_TIMEOUT: Duration = Duration::from_secs(60 * 5);
 pub const WRITE_TIMEOUT: Duration = Duration::from_secs(30);
 
 pub const HTTP_CR_LF: &[u8] = b"\r\n";
